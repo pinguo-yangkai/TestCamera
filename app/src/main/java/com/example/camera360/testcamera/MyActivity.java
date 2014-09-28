cbcc8f76fb4c70c2f5a34bb343bdad9854745eff
pinguo-yangkai <yanglai@camera360.com>
2014-09-28 13:39:04 +0800

    测试是否能提到仓库

--- app/src/main/java/com/example/camera360/testcamera/MyActivity.java
+++ app/src/main/java/com/example/camera360/testcamera/MyActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MyActivity extends Activity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
